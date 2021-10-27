4ccf742f0a0b44502a68c44bfda1f3f2569609df
CHENboxu <1534030849@qq.com>
2021-10-26 23:50:17 -0400

    add view counter and transcript

--- server.js
+++ server.js
@@ -5,68 +5,113 @@ var https = require('https');
 var prefix = "https://xkcd.com"
 var suffix = "/info.0.json";
 var port =  process.env.PORT || 3000;
-
+// save the number of latest comic
 var maxNum = 1;
+//counters save the times that a specific comic has been viewed
+var viewCounter = [0];
+
 
 var app = express();
 app.set('view engine','ejs');
 
-// static resource js/css
+//static resource js/css
 //app.use('/',express.static(path.join(__dirname,'public')));
-//dynamic send back html
-//send current comic 
+
+//get the main page
 app.get('/',(req,res) => {
-    //fetch xkcd
-    getComic(prefix+suffix,res, req);
+    getComic(prefix+suffix,res);
 });
 
+
+function isNum(val){
+    var reg = new RegExp("[0-9]");
+    return reg.test(val);
+}
+//get specific comic page with id in url
 app.get('/:id',(req,res) => {
-    getComic(prefix+"/"+req.params.id+suffix,res,req);
+    if(isNum(req.params.id)){
+        getComic(prefix+"/"+req.params.id+suffix,res);
+    }
+    else{
+        renderError(res);
+    }
 });
 
+//get random comic
 app.get('/random/comic',(req,res) => {
+    //get a random number in [1,currentcomicnumber]
     let randomNum = Math.floor(Math.random()*maxNum)+1;
-    console.log("randomguy"+randomNum);
-    getComic(prefix+"/"+randomNum+suffix,res,req);
+    getComic(prefix+"/"+randomNum+suffix,res);
 });
+
+
 
 app.listen(port, ()=>{
-    console.log('listening at port' + port);
-});
-
-function getComic(url, res, req) {
-    https.get(url, (response) => {
-        console.log("receive json from xkcd"+url);
-        if (response.statusCode != 200) {
-            console.log("falsecode:" + response.statusCode);
-            res.render('page/error', (err,html)=>{
-                res.send(html);
-            });
+    console.log('listening at port:' + port);
+    //get the number of the current comic first!
+    https.get(prefix + suffix,res =>{
+        if(res.statusCode != 200){
+            console.log('cannot connect with xkcd.com');
         }else{
             let data = '';
-            response.on('data', (d) => {
-                data += d;
-            });
-            response.on('end', () => {
-                let parseData = JSON.parse(data);
-                // console.log(parseData);
-                maxNum = maxNum>parseData.num ? maxNum:parseData.num;
-                res.render('page/index', {
-                    year: parseData.year,
-                    month: parseData.month,
-                    day: parseData.day,
-                    img: parseData.img,
-                    alt: parseData.alt,
-                    safetitle: parseData.safe_title,
-                    transcript: parseData.transcript,
-                    prevNum: parseData.num > 1 ? parseData.num - 1 : 1,
-                    nextNum: parseData.num + 1
-                },(err,html)=>{
-                    res.send(html);
-                });
+            res.on('data', (d) => {data += d;});
+            res.on('end', () => {let parseData = JSON.parse(data);
+                if(parseData.num  > maxNum){
+                    viewCounter = Array(parseData.num).fill(0);
+                    maxNum = parseData.num;
+                }
             });
         }
-        
+    })
+});
 
+
+
+function getComic(url, res) {
+    https.get(url, (response) => {
+        console.log("receive json from xkcd:"+url);
+        if (response.statusCode != 200) {
+            console.log("falsecode:" + response.statusCode + "from" + url);
+            renderError(res);
+        }else{
+            renderHtml(response, res);
+        }
     });
+}
+
+function renderHtml(response, res) {
+    let data = '';
+    response.on('data', (d) => {//receive data
+        data += d;
+    });
+    response.on('end', () => {//use data to render ejs and send back the html to client
+        let parseData = JSON.parse(data);
+        // console.log(parseData);
+        // if got new leatest comic
+        if(parseData.num  > maxNum){
+            for(let i=0; i<parseData.num - maxNum; i++){
+                viewCounter.push(0);
+            }
+            maxNum = parseData.num;
+        }
+        if(parseData.num > 0)
+            viewCounter[parseData.num -1]++;
+
+        res.render('page/index', {
+            year: (!parseData.year)? 'unknown':parseData.year,
+            month: (!parseData.month)? 'unknown':parseData.month,
+            day: (!parseData.day)? 'unknown':parseData.day,
+            img: parseData.img,
+            alt: (!parseData.alt)? 'unknown':parseData.alt,
+            safetitle: (!parseData.safe_title)? 'unknown':parseData.safe_title,
+            transcript: (!parseData.transcript)? 'transcript unavialable':parseData.transcript,
+            counter: parseData.num > 0 ? viewCounter[parseData.num -1]:0,
+            prevNum: parseData.num > 1 ? parseData.num - 1 : 1,
+            nextNum: parseData.num + 1
+        });
+    });
+}
+
+function renderError(res) {
+    res.render('page/error');
 }
